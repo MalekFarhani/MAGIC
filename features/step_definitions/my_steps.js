62955c6c58bc310fb22569ed8dc2a317df4208e1
malek <farhani.malek@esprit.tn>
2023-07-20 14:15:54 +0200

    sel

--- features/step_definitions/my_steps.js
+++ features/step_definitions/my_steps.js
@@ -1,7 +1,10 @@
 const {Given, When, Then, Before, After} = require('@cucumber/cucumber')
 const {expect} = require('chai');
-const { By } = require('selenium-webdriver');
+const { By, Builder} = require('selenium-webdriver');
 
+
+
+/*
 When('I add {int} and {int}', function (int, int2)
 {
 
@@ -11,3 +14,35 @@ Then('The result should be {int}', function (int){
 
 
 });
+*/
+let driver = new Builder()
+   .forBrowser('chrome')
+   .build();
+//let driver
+
+//Before(function () {
+  //      driver = new webdriver.Builder().forBrowser('chrome').build();
+   // }
+//)
+
+//After(function () {
+ //   driver.quit();
+//})
+
+Given('I visit google homepage', async () => {
+        await driver.get("https://www.google.com");
+    }
+);
+
+When ('I When I search for Techverito', async () => {
+        await driver.findElement(By.name('q')).sendKeys('Techverito'+'\n')
+    }
+);
+
+Then ('I should see the results', async () => {
+    let text = await driver.findElement(By.id('search')).getText()
+    console.log(text);
+})
+//When(/^I search for Techverito$/, function () {
+
+//});
